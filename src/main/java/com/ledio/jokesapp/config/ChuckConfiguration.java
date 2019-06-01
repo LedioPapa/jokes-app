7098b6e4850420e41794d34a7feeb464598cde3f
Ledio <ledio.papa@fti.edu.al>
2019-06-01 23:55:15 +0300

    Added ChuckNorrisQuotes object as a bean in the spring context by using XML Based Configuration

--- src/main/java/com/ledio/jokesapp/config/ChuckConfiguration.java
+++ src/main/java/com/ledio/jokesapp/config/ChuckConfiguration.java
@@ -1,16 +1,14 @@
 package com.ledio.jokesapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Ledio Papa on 6/1/19
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
